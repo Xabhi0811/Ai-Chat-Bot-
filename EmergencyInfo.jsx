fe05dc57a91a6e45016add34401d4cf1b811f0ae
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-04-02 09:27:49 +0530

    Update EmergencyInfo.jsx

--- EmergencyInfo.jsx
+++ EmergencyInfo.jsx
@@ -4,7 +4,6 @@ export default function EmergencyInfo() {
   const showEmergencyInfo = () => {
     alert("Emergency Contacts:\nPolice: 100\nFire: 101\nAmbulance: 102\nInstant Help: 112");
   };
-
   return (
     <button className="emergency-btn" onClick={showEmergencyInfo}>
       Emergency Info
