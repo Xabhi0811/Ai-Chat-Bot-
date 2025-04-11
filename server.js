df2adaecea9f1b1f5b4fa67d275dc47e9468aaee
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-04-11 21:12:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,5 @@ const app = require('./src/app');
 require('dotenv').config();
 
 app.listen(3000, () => {    
-    console.log('Server is running on port 3000')
-}
+    console.log('Server is running on port 3000') }
 );
