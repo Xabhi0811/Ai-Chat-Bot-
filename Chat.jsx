ab643fb2087f293d4539f7294e73aec7af08bfd7
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-04-14 11:54:10 +0530

    Update Chat.jsx

--- Chat.jsx
+++ Chat.jsx
@@ -20,6 +20,7 @@ export default function Chat() {
 
 
       
+      
       const data = await response.json();
       setMessages([...newMessages, { user: "Chatbot", text: data.reply }]);
     } catch (error) {
