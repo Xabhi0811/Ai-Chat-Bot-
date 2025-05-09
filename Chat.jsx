e48512ad24dfae8588d6f2e5bdc83aa01e9875e6
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-05-09 21:55:59 +0530

    Update Chat.jsx

--- Chat.jsx
+++ Chat.jsx
@@ -10,6 +10,7 @@ export default function Chat() {
     const newMessages = [...messages, { user: "You", text: input }];
     setMessages(newMessages);
     setInput("");
+    
 
     try {
       const response = await fetch("http://localhost:3000/chat", {
